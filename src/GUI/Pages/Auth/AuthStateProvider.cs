27e80848039024d4655669cf5640ae5402f1c85f
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-05-24 19:13:07 +0300

    add login (#3)
    * init
    
    * add api
    
    * update pages
    
    * add session sorage
    
    * fixes
    
    * small fix
    
    * fixes
    
    * add "enter" event
    
    * fix
    
    * added redirect for login
    
    * fixes
    
    Co-authored-by: Spartak Ryabtsev <spartak.r@gmail.com>

---
+++ src/GUI/Pages/Auth/AuthStateProvider.cs
@@ -0,0 +1,31 @@
+﻿using LT.DigitalOffice.AuthService;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GUI.Pages.Auth
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity());
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.FromResult(0);
+
+            return new AuthenticationState(claimsPrincipal);
+        }
+
+        public void LoginNotify(AuthenticationResponse response)
+        {
+            var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, response.UserId.ToString()),
+                },
+                "Real Authentication");
+
+            claimsPrincipal = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
--- src/GUI/Program.cs
+++ src/GUI/Program.cs
@@ -1,12 +1,10 @@
+using GUI.Pages.Auth;
+using LT.DigitalOffice.GUI.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Blazored.SessionStorage;
 
 namespace LT.DigitalOffice.GUI
 {
@@ -16,8 +14,10 @@ namespace LT.DigitalOffice.GUI
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddBlazoredSessionStorage();
 
             await builder.Build().RunAsync();
         }
