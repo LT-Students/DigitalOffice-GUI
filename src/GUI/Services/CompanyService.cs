d0dfae22b777f91116f9e51a709b4ebc0745a1c4
Mariya <mariyasin@list.ru>
2021-05-28 16:10:33 +0300

    add companyService

---
+++ src/GUI/Services/CompanyService.cs
@@ -0,0 +1,53 @@
+﻿using Blazored.SessionStorage;
+using LT.DigitalOffice.Client.Company;
+using LT.DigitalOffice.GUI.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.GUI.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly ISessionStorageService _storage;
+        private readonly CompanyServiceClient _client = new CompanyServiceClient(new System.Net.Http.HttpClient());
+        private string _token;
+        public CompanyService(ISessionStorageService storage)
+        {
+            _storage = storage;
+        }
+
+        public async Task<string> CreateDepartment(NewDepartmentRequest request)
+        {
+            try
+            {
+                _token = await _storage.GetItemAsync<string>("Token");
+                var companyService = new CompanyServiceClient(new System.Net.Http.HttpClient());
+                var response = await companyService.AddDepartmentAsync(request, _token);
+                //var response = await _client.AddDepartmentAsync(request, _token);
+
+                return "Successfully created";
+            }
+            catch (Client.Company.ApiException<Client.Company.ErrorResponse> ex)
+            {
+                return ex.Result.Message;
+            }
+        }
+
+        public async Task<string> CreatePosition(CreatePositionRequest request)
+        {
+            try
+            {
+                _token = await _storage.GetItemAsync<string>("Token");
+                var companyService = new CompanyServiceClient(new System.Net.Http.HttpClient());
+                var response = await companyService.AddPositionAsync(request, _token);
+                //var response = await _client.AddDepartmentAsync(request, _token);
+
+                return "Successfully created";
+            }
+            catch (Client.Company.ApiException<Client.Company.ErrorResponse> ex)
+            {
+                return ex.Result.Message;
+            }
+        }
+    }
+}
+
