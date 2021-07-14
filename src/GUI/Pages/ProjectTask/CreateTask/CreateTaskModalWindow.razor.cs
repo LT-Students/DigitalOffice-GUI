742e953afad221d909a8266200c130c294f104fb
djino <malkinevgeniy437@gmail.com>
2021-07-15 01:47:08 +0300

    Fixed

--- src/GUI/Pages/ProjectTask/CreateTask/CreateTaskModalWindow.razor.cs
+++ src/GUI/Pages/ProjectTask/CreateTask/CreateTaskModalWindow.razor.cs
@@ -92,7 +92,7 @@ namespace LT.DigitalOffice.GUI.Pages.ProjectTask.CreateTask
             }
         }
 
-        private async Task HandleSubmit()
+        public async Task HandleSubmit()
         {
             await _ProjectService.CreateTaskAsync(_taskRequest);
         }
