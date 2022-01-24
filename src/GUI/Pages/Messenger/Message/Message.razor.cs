142793e2005616be11c92d89afd63116d98b063c
Mariya <63921256+MariyaSin@users.noreply.github.com>
2022-01-24 14:02:00 +0300

    Feature/add channels (#30)
    * add messages
    
    * fix states
    
    * remove comments
    
    * add chatHub connection
    
    * fix
    
    * add hub
    
    * add hub to channel
    
    * added scroll
    
    * fixes

--- src/GUI/Pages/Messenger/Message/Message.razor.cs
+++ src/GUI/Pages/Messenger/Message/Message.razor.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Reflection.Metadata;
-using System.Threading.Tasks;
-using LT.DigitalOffice.GUI.Services.ApiClients.MessageService;
+﻿using LT.DigitalOffice.GUI.Services.ApiClients.MessageService;
 using Microsoft.AspNetCore.Components;
 
 namespace LT.DigitalOffice.GUI.Pages.Messenger.Message
@@ -9,28 +6,6 @@ namespace LT.DigitalOffice.GUI.Pages.Messenger.Message
   public partial class Message
   {
     [Parameter]
-    public Guid ChannelId { get; set; }
-
-    private CreateMessageRequest _request = new();
-
-
-    private async Task SendMessageAsync()
-    {
-      try
-      {
-        _request.Status = StatusType.Sent;
-        _request.ChannelId = ChannelId;
-        OperationResultResponse response = await _messageService.CreateMessageAsync(_request);
-
-        if (response.Status == OperationResultStatusType.FullSuccess)
-        {
-          _request.Content = "";
-          return;
-        }
-      }
-      catch (Exception ex)
-      {
-      }
-    }
+    public MessageInfo MessageData { get; set; }
   }
 }
